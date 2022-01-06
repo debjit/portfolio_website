e7ab88e0cffd5a20c1b1e76d70caf1e23b421ec7
Debjit <debjit012@gmail.com>
2022-01-06 16:27:47 +0530

    WIP

--- src/pages/test1.js
+++ src/pages/test1.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
-const FetchData = () => {
+function getApiData() {
     const [data, setData] = useState('')
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);       
+        setData(json);
       });
+}
+
+const FetchData = () => {
+    
       
     const data2 = {
         name: 'debjit'
@@ -14,6 +19,17 @@ const FetchData = () => {
       return (
         <div>
         Name: {data2.name}
+        <button onClick={getApiData}>Fetch API Data</button>
+        <div>
+        <br />
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.userId},{item.title}
+            </li>
+          ))}
+        </ul>
+      </div>
 
         </div>
       )
